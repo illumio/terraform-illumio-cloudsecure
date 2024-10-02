ae77c3f549f07cc5f84022cf22ad6fc89cba9574
Romain Lenglet <romain.lenglet@berabera.info>
2024-10-02 20:48:24 +0000

    Update the module version in all examples

--- examples/k8s_cluster/main.tf
+++ examples/k8s_cluster/main.tf
@@ -10,7 +10,7 @@ provider "illumio-cloudsecure" {
 }
 
 module "k8s_cluster" {
-  source            = "github.com/illumio/terraform-illumio-cloudsecure//modules/k8s_cluster?ref=v1.0.0"
+  source            = "github.com/illumio/terraform-illumio-cloudsecure//modules/k8s_cluster?ref=v1.1.0"
   illumio_region    = "aws-us-west-2"
   name              = "example-release"
   description       = "Dev cluster in aws-us-west-2"

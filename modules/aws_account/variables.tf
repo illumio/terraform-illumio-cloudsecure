48cc7ea4a216fa1b549a9e1e30c296116f7e96da
Romain Lenglet <romain.lenglet@berabera.info>
2024-10-02 20:18:50 +0000

    Update variables.tf

--- modules/aws_account/variables.tf
+++ modules/aws_account/variables.tf
@@ -1,20 +1,20 @@
-variable "iam_role_name" {
-  description = "Customize the name of IAM role for Illumio AWS integration"
+variable "iam_name_prefix" {
+  description = "The prefix given to all AWS IAM resource names."
   type        = string
-  default     = "IllumioCloudIntegrationRole"
+  default     = "IllumioCloudIntegration"
   validation {
-    condition     = length(var.iam_role_name) > 1
-    error_message = "The IAM role name cannot be empty."
+    condition     = length(var.iam_name_prefix) > 1
+    error_message = "The iam_name_prefix value must not be empty."
   }
 }
 
 variable "mode" {
-  description = "integration mode Read/ReadWrite."
+  description = "The account's access mode, must be \"ReadWrite\" (default) or \"Read\"."
   type        = string
   default     = "ReadWrite"
   validation {
     condition     = contains(["Read", "ReadWrite"], var.mode)
-    error_message = "Must be Read or ReadWrite"
+    error_message = "The mode value must be \"ReadWrite\" or \"Read\"."
   }
 }
 
@@ -35,4 +35,10 @@ variable "name" {
     condition     = length(var.name) > 1
     error_message = "The name value must not be empty."
   }
+}
+
+variable "tags" {
+  description = "The optional tags added to every configured AWS resource."
+  type        = map(string)
+  default     = {}
 }

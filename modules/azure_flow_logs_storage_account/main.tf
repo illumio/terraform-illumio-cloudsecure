806115318857dc9c6c092caa0390fc457bdd2691
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2024-10-29 15:20:52 -0700

    azure flow logs from storage accounts

---
+++ modules/azure_flow_logs_storage_account/main.tf
@@ -0,0 +1,52 @@
+data "azurerm_subscription" "current" {}
+
+locals {
+  storage_accounts_map = {
+    for sa in var.storage_accounts : "${sa.name}-${sa.resource_group_name}" => sa
+  }
+}
+
+# Retrieve storage account scopes (for existing storage accounts)
+data "azurerm_storage_account" "account" {
+  for_each          = local.storage_accounts_map
+  name                = each.value.name
+  resource_group_name = each.value.resource_group_name
+}
+
+# Role Definitions for NSG
+resource "azurerm_role_definition" "illumio_storage_reader" {
+  name        = "${var.iam_name_prefix}SRRole"
+  description = "Illumio Storage Reader role"
+
+  permissions {
+    actions = [
+      "Microsoft.Storage/storageAccounts/blobServices/containers/read",
+      "Microsoft.Storage/storageAccounts/blobServices/generateUserDelegationKey/action"
+    ]
+    data_actions = [
+      "Microsoft.Storage/storageAccounts/blobServices/containers/blobs/read"
+    ]
+  }
+
+  assignable_scopes = ["/subscriptions/${data.azurerm_subscription.current.subscription_id}"]
+  scope             = "/subscriptions/${data.azurerm_subscription.current.subscription_id}"
+}
+
+
+resource "azurerm_role_assignment" "illumio_storage_reader_assignment" {
+  for_each = data.azurerm_storage_account.account
+  principal_id       = var.service_principal_client_id
+  description        = "Illumio NSG role assignment"
+  role_definition_id = azurerm_role_definition.illumio_storage_reader.role_definition_resource_id
+  scope              = each.value.id
+}
+
+
+resource "illumio-cloudsecure_azure_flow_logs_storage_account" "storage_accounts" {
+  for_each = data.azurerm_storage_account.account
+  subscription_id             = data.azurerm_subscription.current.subscription_id
+  storage_account_resource_id = each.value.id
+  depends_on = [
+    azurerm_role_assignment.illumio_storage_reader_assignment
+  ]
+}
---
+++ modules/azure_flow_logs_storage_account/variables.tf
@@ -0,0 +1,27 @@
+variable "iam_name_prefix" {
+  description = "The prefix given to all Azure resource names."
+  type        = string
+  default     = "IllumioCloudIntegration"
+  validation {
+    condition     = length(var.iam_name_prefix) > 0
+    error_message = "The iam_name_prefix value must not be empty."
+  }
+}
+
+variable "service_principal_client_id" {
+  type = string
+  description = "The client ID of the service principal used to access the storage accounts."
+  validation {
+    condition     = length(var.service_principal_client_id) > 0
+    error_message = "The service_principal_client_id value must not be empty."
+  }
+}
+
+variable "storage_accounts" {
+  type    = list(object({
+    name = string
+    resource_group_name = string
+  }))
+  description = "The list of storage accounts to be monitored."
+  default = []
+}

538f9879e35bdf1cc084f76695eb164d2dec19d7
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2024-11-07 14:46:00 -0800

    remove get perms in list

--- demoi/main.tf
+++ demoi/main.tf
@@ -4,11 +4,27 @@ locals {
   arns_with_path_suffix = [for arn in var.s3_bucket_arns : arn if !can(regex("^[^/]+$", arn))]
   arns_of_buckets_with_path_suffix = [for arn in local.arns_with_path_suffix : regex("^[^/]+", arn)]
 }
+# output "a1_fullpaths" {
+#   value = local.full_bucket_arns
+# }
+# output "a2_fullpaths_suffixed" {
+#   value = local.full_bucket_arns_with_suffix
+# }
+# output "a3_bucket_arns_from_arns_with_path" {
+#   value = local.bucket_arns_from_arns_with_path
+# }
+# output "a4_arns_with_path_suffixed" {
+#   value = local.arns_with_path_suffixed
+# }
+# output "a5_arns_with_path" {
+#   value = local.arns_with_path
+# }
+# output "a6_map_of_arn_path_from_arns_with_path" {
+#   value = local.map_of_arn_path_from_arns_with_path
+# }
 
-resource "aws_iam_role_policy" "s3_bucket_list" {
-  name   = "${var.iam_name_prefix}BucketListPolicy"
-  role   = var.role_id
-  policy = jsonencode({
+output "demo" {
+  value = jsonencode({
     Version   = "2012-10-17"
     Statement = concat([
       # Allow for entire bucket arns
@@ -28,8 +44,8 @@ resource "aws_iam_role_policy" "s3_bucket_list" {
         Resource = local.arns_of_buckets_with_path_suffix
       }
     ],
-    [
-      for arn in local.arns_with_path_suffix : {
+      [
+        for arn in local.arns_with_path_suffix : {
         Effect = "Allow"
         Action = [
           "s3:ListBucket",
@@ -43,14 +59,12 @@ resource "aws_iam_role_policy" "s3_bucket_list" {
           }
         }
       }
-    ])
+      ])
   })
 }
 
-resource "aws_iam_role_policy" "s3_bucket_read" {
-  name   = "${var.iam_name_prefix}BucketReadPolicy"
-  role   = var.role_id
-  policy = jsonencode({
+output "read" {
+  value = jsonencode({
     Version   = "2012-10-17"
     Statement = concat([
       {
@@ -64,10 +78,19 @@ resource "aws_iam_role_policy" "s3_bucket_read" {
   })
 }
 
-data "aws_caller_identity" "current" {}
 
-resource "illumio-cloudsecure_aws_flow_logs_s3_bucket" "bucket_example" {
-  for_each      = var.s3_bucket_arns
-  account_id    = data.aws_caller_identity.current.account_id
-  s3_bucket_arn = each.key
+variable "s3_bucket_arns" {
+  description = "The set of ARNs of AWS S3 buckets containing flow logs."
+  type        = set(string)
+  default = [
+    "arn:aws:s3:::flows-bucket-1",
+    "arn:aws:s3:::flows-bucket-2",
+    "arn:aws:s3:::flows-bucket-3",
+    "arn:aws:s3:::flows-bucket-4/custom-path/first",
+    "arn:aws:s3:::flows-bucket-5/custom-path/second"
+  ]
+  validation {
+    condition     = length(var.s3_bucket_arns) > 0
+    error_message = "The s3_bucket_arns value must not be empty."
+  }
 }

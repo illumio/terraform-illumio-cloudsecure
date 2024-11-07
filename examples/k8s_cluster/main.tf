3d47a0a8845069d50269cdbd2326e3fc9cc803b5
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2024-11-07 14:24:47 -0800

    support for s3 buckets with path

--- examples/k8s_cluster/main.tf
+++ examples/k8s_cluster/main.tf
@@ -11,7 +11,7 @@ provider "illumio-cloudsecure" {
 
 module "k8s_cluster_dev" {
   source         = "illumio/cloudsecure/illumio//modules/k8s_cluster"
-  version        = "1.3.0"
+  version        = "1.3.1"
   illumio_region = "aws-us-west-2"
   name           = "example-release"
   description    = "Dev cluster in aws-us-west-2"

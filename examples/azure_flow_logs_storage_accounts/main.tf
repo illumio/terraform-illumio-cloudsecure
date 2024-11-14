c8252d260502f8ac88fc3dd31a9282efd72a9de6
shireesh gadidesi <shireesh.gadidesi@illumio.com>
2024-11-13 16:46:07 -0800

    fixes in examples

--- examples/azure_flow_logs_storage_accounts/main.tf
+++ examples/azure_flow_logs_storage_accounts/main.tf
@@ -1,8 +1,16 @@
-provider "aws" {
-  region = "us-west-1"
+provider "azurerm" {
+  features {}
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
 }
 
-provider "time" {}
+provider "azuread" {
+  client_id       = var.azure_client_id
+  client_secret   = var.azure_client_secret
+  tenant_id       = var.azure_tenant_id
+}
 
 provider "illumio-cloudsecure" {
   client_id     = var.illumio_cloudsecure_client_id
--- examples/azure_flow_logs_storage_accounts/variables.tf
+++ examples/azure_flow_logs_storage_accounts/variables.tf
@@ -16,3 +16,41 @@ variable "illumio_cloudsecure_client_secret" {
     error_message = "The illumio_cloudsecure_client_secret value must not be empty."
   }
 }
+
+
+variable "azure_subscription_id" {
+  type        = string
+  description = "The Azure Subscription ID."
+  validation {
+    condition     = length(var.azure_subscription_id) > 0
+    error_message = "The azure_subscription_id value must not be empty."
+  }
+}
+
+variable "azure_client_id" {
+  type        = string
+  description = "The Azure Client ID."
+  validation {
+    condition     = length(var.azure_client_id) > 0
+    error_message = "The azure_client_id value must not be empty."
+  }
+}
+
+variable "azure_client_secret" {
+  type        = string
+  sensitive   = true
+  description = "The Azure Client Secret."
+  validation {
+    condition     = length(var.azure_client_secret) > 0
+    error_message = "The azure_client_secret value must not be empty."
+  }
+}
+
+variable "azure_tenant_id" {
+  type        = string
+  description = "The Azure Tenant ID."
+  validation {
+    condition     = length(var.azure_tenant_id) > 0
+    error_message = "The azure_tenant_id value must not be empty."
+  }
+}

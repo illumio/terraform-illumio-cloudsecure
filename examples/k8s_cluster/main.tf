97ae4542e85ad79198e21afa4b63cb9352084a93
Evan Chopra <evan.chopra@illumio.com>
2025-04-01 17:09:02 -0700

    updating version and fixing version-> operator_version

--- examples/k8s_cluster/main.tf
+++ examples/k8s_cluster/main.tf
@@ -1,6 +1,6 @@
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"  # Adjust this path as needed
+    config_path = "~/.kube/config" # Adjust this path as needed
   }
 }
 
@@ -10,9 +10,9 @@ provider "illumio-cloudsecure" {
 }
 
 module "k8s_cluster_dev" {
-  source         = "illumio/cloudsecure/illumio//modules/k8s_cluster"
-  version        = "1.4.3"
-  illumio_region = "aws-us-west-2"
-  name           = "example-release"
-  description    = "Dev cluster in aws-us-west-2"
+  source           = "illumio/cloudsecure/illumio//modules/k8s_cluster"
+  operator_version = "1.4.3"
+  illumio_region   = "aws-us-west-2"
+  name             = "example-release"
+  description      = "Dev cluster in aws-us-west-2"
 }

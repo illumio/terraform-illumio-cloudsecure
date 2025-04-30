7d45847180aa7967a2b98bdb55c8a734caf7b40f
aaron.nguyen <aaron.nguyen@illumio.com>
2025-04-29 18:01:31 -0700

    https proxy support

--- modules/k8s_cluster/variables.tf
+++ modules/k8s_cluster/variables.tf
@@ -10,6 +10,12 @@ variable "enable_falco" {
   default     = false
 }
 
+variable "https_proxy" {
+  description = "The HTTPS proxy to be used by the cloud-operator. If not set, no proxy will be used."
+  type        = string
+  default     = ""
+}
+
 variable "illumio_region" {
   description = "Illumio Region where the k8s cluster will be onboarded. An Illumio Region is a designated cloud region where the CloudSecure cloud-operator deployed in the k8s cluster connects after onboarding. Choose the Illumio Region nearest to the k8s cluster to maximize performance and security. Must be one of: `aws-ap-southeast-2`, `aws-eu-west-2`, `aws-us-west-2`, `aws-us-west-1`, `aws-eu-west-2`, `azure-us-east-2`, `azure-germany-west-central`, `azure-us-west-2`."
   type        = string

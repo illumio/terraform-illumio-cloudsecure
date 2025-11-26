eb7a939e704b857917471174b35fde8b13f46591
Romain Lenglet <romain.lenglet@berabera.info>
2025-11-26 08:49:18 +0000

    Set version to v1.5.5

--- examples/aws_account/main.tf
+++ examples/aws_account/main.tf
@@ -9,7 +9,7 @@ provider "illumio-cloudsecure" {
 
 module "aws_account_dev" {
   source  = "illumio/cloudsecure/illumio//modules/aws_account"
-  version = "1.5.4"
+  version = "1.5.5"
   name    = "Test Account"
   tags    = {
     Name  = "CloudSecure Account Policy"

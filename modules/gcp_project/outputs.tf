f0aed6b22bfe5e98b60677dd9e890b3e17cd6350
Param Patel <51967918+parampatel21@users.noreply.github.com>
2025-12-10 20:52:14 +0000

    add depends on

--- modules/gcp_project/outputs.tf
+++ modules/gcp_project/outputs.tf
@@ -1,6 +1,7 @@
 output "service_account_email" {
   value       = google_service_account.illumio_sa.email
   description = "The email of the created service account created for Illumio CloudSecure under the provided GCP Project ID."
+  depends_on  = [illumio-cloudsecure_gcp_project.project]
 }
 
 output "mode" {

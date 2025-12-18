40730acb7ded1cf9357850b381f220bfe57cc8cf
Romain Lenglet <romain.lenglet@berabera.info>
2025-12-18 08:11:31 +0000

    Delete unnecessary outputs.tf from the gcp_flow_logs_pubsub_topic submodule

--- modules/gcp_flow_logs_pubsub_topic/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "project_role_id" {
-  value       = google_project_iam_custom_role.pubsub_access_role.id
-  description = "The ID of the custom IAM role created for Pub/Sub subscription management."
-}
-
-output "topic_role_id" {
-  value       = google_project_iam_custom_role.flow_access_role.id
-  description = "The ID of the custom IAM role created for attaching subscriptions to GCP Pub/Sub topics."
-}

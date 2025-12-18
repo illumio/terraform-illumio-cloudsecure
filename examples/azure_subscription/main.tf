8cc0d182afd8175b57749ee72d3a832851ccd5e7
Romain Lenglet <romain.lenglet@berabera.info>
2025-12-18 08:42:25 +0000

    Set version to v1.6.0 in examples

--- examples/azure_subscription/main.tf
+++ examples/azure_subscription/main.tf
@@ -19,7 +19,7 @@ provider "illumio-cloudsecure" {
 
 module "azure_subscription_dev" {
   source                 = "illumio/cloudsecure/illumio//modules/azure_subscription"
-  version                = "1.5.5"
+  version                = "1.6.0"
   name                   = "Test Azure Subscription"
   mode                   = "ReadWrite"
   secret_expiration_days = 365
--- examples/k8s_cluster/main.tf
+++ examples/k8s_cluster/main.tf
@@ -11,7 +11,7 @@ provider "illumio-cloudsecure" {
 
 module "k8s_cluster_dev" {
   source           = "illumio/cloudsecure/illumio//modules/k8s_cluster"
-  version          = "1.5.5"
+  version          = "1.6.0"
   illumio_region   = "aws-us-west-2"
   name             = "example-release"
 

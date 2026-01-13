d8d465dfa330ba218df6a1bac3c707cb4cf4af46
Romain Lenglet <romain.lenglet@berabera.info>
2026-01-13 01:19:09 +0000

    Set version to v1.6.2

--- examples/azure_subscription/main.tf
+++ examples/azure_subscription/main.tf
@@ -19,7 +19,7 @@ provider "illumio-cloudsecure" {
 
 module "azure_subscription_dev" {
   source                 = "illumio/cloudsecure/illumio//modules/azure_subscription"
-  version                = "1.6.1"
+  version                = "1.6.2"
   name                   = "Test Azure Subscription"
   mode                   = "ReadWrite"
   secret_expiration_days = 365
